89d7c9823e3cb6f5c8c544bcc99e33802d057fdf
Kushagra bharti <kushagrabharti04@gmail.com>
2026-01-08 21:56:34 +0530

    Fix SQL syntax errors in intermediate and advanced queries

--- Day 2/Practice Sql-1.sql
+++ Day 2/Practice Sql-1.sql
@@ -11,6 +11,7 @@ CREATE TABLE courses (
     course_meta JSONB,
     enrolled_on TIMESTAMP
 );
+
 INSERT INTO courses (
     student_name,
     email,
@@ -80,72 +81,48 @@ SELECT * FROM courses
 SELECT student_name, course_name FROM courses
 
 -- Q2 --
-SELECT * 
-FROM courses
-WHERE level = 'Beginner'
+SELECT * FROM courses WHERE level = 'Beginner'
 
 -- Q3 --
-SELECT * 
-FROM courses
-WHERE price > 9999.51
+SELECT * FROM courses WHERE price > 9999.51
 
 -- Q4 --
-SELECT DISTINCT course_name
-FROM courses
+SELECT DISTINCT course_name FROM courses
 
 -- Q5 --
-SELECT * 
-FROM courses
-ORDER BY price  DESC
-LIMIT 5;
+SELECT * FROM courses ORDER BY price DESC LIMIT 5;
 
 ----------------------------------- Now Intermidiate Ques ---------------------------------------
 -- Q6 --
-SELECT *
-FROM courses
-WHERE rating IN (3,4);
+SELECT * FROM courses WHERE rating IN (3, 4);
 
 -- Q7 --
-SELECT *
-FROM courses
-WHERE price BETWEEN 9000 AND 15000;
+SELECT * FROM courses WHERE price BETWEEN 9000 AND 15000;
 
 -- Q8 --
-SELECT * 
-FROM courses
-WHERE student_name LIKE 'T%';
+SELECT * FROM courses WHERE student_name LIKE 'T%';
 
 -- Q9 --
-SELECT * 
-FROM courses
-WHERE completion_status = 'True';
+SELECT * FROM courses WHERE completion_status = 'True';
 
 -- Q10 --
-SELECT *
-FROM courses
-WHERE 'JavaScript' = ANY (skills);
+SELECT * FROM courses WHERE 'JavaScript' = ANY (skills);
 
 --------------------- Advance --------------
 -- Q11 --
 SELECT SUM(price) AS Total_revenue FROM courses;
 
 -- Q12 --
-SELECT AVG(price) As Every_Level
-FROM courses
-GROUP BY level;
+SELECT AVG(price) As Every_Level FROM courses GROUP BY level;
 
 -- Q13 --
-SELECT * 
-FROM courses
-WHERE course_meta->> 'duration' = '10 months'
+SELECT * FROM courses WHERE course_meta ->> 'duration' = '10 months'
 
 -- Q14 --
-SELECT *
-FROM courses
-WHERE array_length(skills, 1) >= 4;
+SELECT * FROM courses WHERE array_length (skills, 1) >= 4;
 
 -- Q15 --
 SELECT completion_status, COUNT(*)
 FROM courses
-GROUP BY completion_status;
-
+GROUP BY
+    completion_status;
